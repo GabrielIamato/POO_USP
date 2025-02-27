b1c81237b86fbede2cf288261a141ac697daedb0
Gabriel Campanelli Iamato <114399483+GabrielIamato@users.noreply.github.com>
2025-02-27 09:33:00 -0300

    Add files via upload

---
+++ aulas_praticas/POO01/src/main/java/com/mycompany/poo01/Produto.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo01;
+
+/**
+ *
+ * @author 15452920
+ */
+public class Produto {
+    private String nome; // 
+    private float preco;/**/
+    private int qtd;
+    final int MAXESTOQUE = 2200;
+    // Construtor: não vai mudar nada existir ou não, pois o NetBeans já implementa
+    public Produto(){
+        System.out.println("Produto Criado");
+    }
+    // Construtor que inicializa as variáveis
+    public Produto(String umNome, float umPreco, int umaQtd){
+        nome = umNome;
+        preco = umPreco;
+        qtd = umaQtd;
+    }
+   
+    // Métodos Get 
+    public int getQtd(){
+        return qtd;
+    }
+    
+    public float getPreco(){
+        return preco;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void print(){
+        System.out.println("Nome: " + nome + ", preco: " + preco+ ", qtd: " +qtd);
+    }
+    
+    public void adicionarEstoque(int quantidade){
+        if(qtd+quantidade < MAXESTOQUE){
+            qtd+= quantidade;
+        }else{
+            System.out.println("Máximo de Estoque");
+        }
+    }
+    
+    public void removerEstoque(int quantidade){
+        if(qtd - quantidade>0){
+            qtd-=quantidade;
+        }else{
+            System.out.println("Estoque insuficiente");
+        }
+    }
+}
